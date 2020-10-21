6add56a3d13efe29edc166a8a03569664e500b92
jaemyeong-hwnag <woaudsla@gmail.com>
2020-10-21 22:37:31 +0900

    Tour 클래스 기초 작업

---
+++ app/Controllers/Tour.php
@@ -0,0 +1,33 @@
+<?php namespace App\Controllers;
+
+class Tour extends BaseController
+{
+	public function index()
+	{
+
+	}
+
+    /**
+     * view tour product
+     * @param int $no
+     * @return \CodeIgniter\HTTP\RedirectResponse|string
+     */
+	public function detail( int $no ){
+        if(!$no) return redirect()->to( './home' );
+        $data['row'] = $this->get_row($no);
+
+        return view( 'tourView', $data);
+    }
+
+	//--------------------------------------------------------------------
+
+    /**
+     * select DB tour in no field
+     * @param $no
+     * @return array
+     */
+    private function get_row( $no ){
+        $tourModel = model('TourModel');
+        return $tourModel->find( $no );
+    }
+}
