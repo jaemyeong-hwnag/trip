1cb60227dab196d18a63068dfd3fa389db4b07b4
jaemyeong-hwnag <woaud2sla@gmail.com>
2021-03-07 04:17:29 +0900

    여행상품 리스트 추가

--- app/Models/TourModel.php
+++ app/Models/TourModel.php
@@ -14,7 +14,6 @@ class TourModel extends Model
      */
     public function getTourView(int $tour_no)
     {
-        $builder = $this->builder();
         // 기본 셋팅
         $data = array(); // return 데이터
 
@@ -46,5 +45,44 @@ class TourModel extends Model
         $data = $this->first();
 
         return $data;
+    }
+
+    public function getTourList($data) {
+        $search_word = $data["search_word"]; // 검색어
+        $offset = $data["offset"]; // 페이지
+        $listCount = 4;
+
+        // from
+        $table = "tour as t"; // table명
+
+        // select
+        $select_array = array();
+        array_push($select_array, "t.tour_no as tour_no");
+        array_push($select_array, "t.tour_name as tour_name");
+        array_push($select_array, "t.tour_price as tour_price");
+        array_push($select_array, "t.tour_picture as tour_picture");
+        array_push($select_array, "t.tour_text as tour_text");
+
+        // 쿼리문 셋팅
+        $builder = $this->builder($table);
+        $builder->select($select_array); // select
+        if($search_word != null) {
+            $builder->orlike('t.tour_name', $search_word, "both"); // where
+        }
+        $builder->limit($listCount, $offset); // limit
+        $builder->orderBy("t.tour_no", "desc");
+
+        $tourCount = $builder->countAllResults(false);
+
+        $db_result = $builder->get(); // 쿼리 실행
+		$db_list = $db_result->getResult();
+
+        // 넘길 데이터 정리
+		$model_result = array();
+		$model_result["result"] = true; // 성공여부
+		$model_result["db_list"] = $db_list; // 조회한 데이터
+		$model_result["pageCount"] = ceil($tourCount/$listCount); // 총 리스트 수
+        
+        return $model_result;
     }
 }
---
+++ app/Views/tour/tourList.php
@@ -0,0 +1,36 @@
+<?= $this->extend('_layout/index') ?>
+<?= $this->section('content') ?>
+<div>
+	<div class="col-3"></div>
+	<div class="alert alert-success" role="alert">여행상품</div>
+	<form id="frm" name="frm" class="form-horizontal"  onSubmit="return false;">
+		<input type="hidden" id="page" name="page" value="">
+		<div class="input-group-prepend">
+			<span class="input-group-text">상품명</span>
+		</div>
+		<input type="text" name="search_word" value="" class="form-control">
+		<button class="btn btn-primary" onClick="javascript:tourListGet(1);">검색</button>
+	<form>
+	<div name="tour_list" id="tour_list">
+
+	</div>
+</div>
+
+<script>
+	function tourListGet(page) {
+		document.getElementById("page").value = page;
+		$.ajax({
+			url: "/tour/tourListGet",
+			type: "POST",
+			dataType: "html",
+			async: true,
+			data: $("#frm").serialize(),
+			success: function(html_result) {
+				document.getElementById("tour_list").innerHTML = html_result;
+			}
+		});
+	}
+
+    tourListGet(1);
+</script>
+<?= $this->endSection() ?>
